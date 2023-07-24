f7b3ec4666f463800efe5577a18d754f77fe7a90
NazaKessel <naza.kessel30@gmail.com>
2023-07-24 13:45:23 -0300

    subiendo cambios

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+const path = require("path");
 const express = require("express");
+
 const app = express();
 
-app.listen(3030, ()=> {
-    console.log("servidor levantado");
-})
-app.get("/index", function(req, res){
-    res.send("bienvenidos al sitio");
+const PORT=process.env.PORT || 3030;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`inicio ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/index.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/registro.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
+
+app.post("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
 });

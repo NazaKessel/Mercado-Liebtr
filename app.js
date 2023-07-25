4b5239ab2ca4decf1a822a1de143ac34c528ef34
NazaKessel <naza.kessel30@gmail.com>
2023-07-25 16:00:26 -0300

    subiendo archivos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT=process.env.PORT || 3030;
+const PORT=process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
